e1589de334a799e9de692166f466f1075098aa52
slapec <pinnacle>
2014-06-29 13:10:12 +0200

    Initial release

---
+++ SDRSharpPluginManager/PluginManagerWindow.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace SDRSharpPluginManager {
+    partial class PluginManagerWindow {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // PluginManagerWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "PluginManagerWindow";
+            this.Text = "SDRSharp Plugin Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
