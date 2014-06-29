d521bb706c9ec455f642b7b1b18167001bb9b76b
slapec <slapec@gmail.com>
2014-06-29 15:18:51 +0200

    Added sdr# folder select on init

--- SDRSharpPluginManager/PluginManagerWindow.cs
+++ SDRSharpPluginManager/PluginManagerWindow.cs
@@ -1,17 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace SDRSharpPluginManager {
     public partial class PluginManagerWindow : Form {
+        public enum ProcessResult {
+            Success = 0, 
+            Missing = 1
+        };
+        public static String[] RequiredFiles = { "SDRSharp.Common.dll" };
+
+        private FolderBrowserDialog sdrSharpFolderDialog;
+
         public PluginManagerWindow() {
             InitializeComponent();
         }
+
+        private ProcessResult ProcessDirectory(String path) {
+            Directory.SetCurrentDirectory(path);
+
+            foreach (String requiredFileName in RequiredFiles) {
+                if (!File.Exists(requiredFileName)) {
+                    String msg = String.Format("File '{0}' is required to run this program but not found in {1}", requiredFileName, path);
+
+                    MessageBox.Show(msg, "Missing file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return ProcessResult.Missing;
+                }
+            }
+            return ProcessResult.Success;
+        }
+
+        #region Set cwd to SDRSharp's directory
+        private void PluginManagerWindow_Load(object sender, EventArgs e) {
+            DialogResult dialogResult;
+            ProcessResult processResult;
+
+            sdrSharpFolderDialog = new FolderBrowserDialog();
+            sdrSharpFolderDialog.Description = "Select the location of SDR#";
+            sdrSharpFolderDialog.SelectedPath = Directory.GetCurrentDirectory();
+            sdrSharpFolderDialog.ShowNewFolderButton = false;
+
+            processResult = ProcessDirectory(sdrSharpFolderDialog.SelectedPath);
+            while (processResult != ProcessResult.Success) {
+                dialogResult = sdrSharpFolderDialog.ShowDialog();
+                if (dialogResult == DialogResult.Cancel) {
+                    this.Close();
+                    break;
+                }
+                else {
+                    processResult = ProcessDirectory(sdrSharpFolderDialog.SelectedPath);
+                }
+            }
+            MessageBox.Show("Show UI");
+        }
+        #endregion
     }
 }
