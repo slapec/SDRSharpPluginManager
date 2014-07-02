751acca4a55f6e3dcaa472e848ec023dc1a7f65f
slapec <slapec@gmail.com>
2014-07-02 11:31:13 +0200

    LoadConfig works without XML now. Some variable name changes and comments

--- SDRSharpPluginManager/PluginManagerWindow.cs
+++ SDRSharpPluginManager/PluginManagerWindow.cs
@@ -2,20 +2,23 @@
 using System.IO;
 using System.Windows.Forms;
 using System.Configuration;
-using System.Xml;
 using System.Reflection;
 using System.Linq;
+using System.Collections.Specialized;
 
 namespace SDRSharpPluginManager {
     public partial class PluginManagerWindow : Form {
         public enum ProcessResult {Success = 0, Missing = 1};
-        public static String SDRSharpCommonDllFName = "SDRSharp.Common.dll";
-        public static String SDRSharpExe = "SDRSharp.exe.Config";
-        public static String[] RequiredFiles = {SDRSharpExe, SDRSharpCommonDllFName};
-        public static String PluginInterfaceName = "ISharpPlugin";
-        public static String DisplayNameFieldName = "_displayName";
 
-        public static String PluginFileFilter = "SDRSharp Plugins (*.dll)|*.dll";
+        public static string ConfigFileName = "SDRSharp.exe.Config";
+        public static string[] RequiredFiles = {ConfigFileName, 
+                                                "SDRSharp.Common.dll",
+                                                "SDRSharp.Radio.dll"};
+
+        public static string PluginInterfaceName = "ISharpPlugin";
+        public static string DisplayNameFieldName = "_displayName";
+
+        public static string PluginFileFilter = "SDRSharp Plugins (*.dll)|*.dll";
 
         private FolderBrowserDialog sdrSharpFolderDialog;
         private OpenFileDialog pluginFileDialog;
@@ -27,13 +30,14 @@ namespace SDRSharpPluginManager {
             currentDomain.AssemblyResolve += currentDomain_AssemblyResolve;
         }
 
+        // This handler is needed to satisfy the dependencies of a parsed plugin
         Assembly currentDomain_AssemblyResolve(object sender, ResolveEventArgs args) {
-            AssemblyName name = new AssemblyName(args.Name);
-            String assemblyNameWithExtension = name.Name + ".dll";
+            AssemblyName unresolvedAssemblyName = new AssemblyName(args.Name);
+            string unresolvedAssemblyNameWithExtension = unresolvedAssemblyName.Name + ".dll";
 
-            String assemblyAbsolutePath = Path.Combine(Directory.GetCurrentDirectory(), assemblyNameWithExtension);
+            string unresolvedAssemblyAbsolutePath = Path.Combine(Directory.GetCurrentDirectory(), unresolvedAssemblyNameWithExtension);
 
-            return Assembly.LoadFrom(assemblyAbsolutePath);
+            return Assembly.LoadFrom(unresolvedAssemblyAbsolutePath);
         }
 
         #region Parsing config file
@@ -41,12 +45,14 @@ namespace SDRSharpPluginManager {
             DialogResult dialogResult;
             ProcessResult processResult;
 
+            // Folder dialog is here so it is shown right on running this program
             sdrSharpFolderDialog = new FolderBrowserDialog();
             sdrSharpFolderDialog.Description = "Select the location of SDR#";
             sdrSharpFolderDialog.SelectedPath = Directory.GetCurrentDirectory();
             sdrSharpFolderDialog.ShowNewFolderButton = false;
 
             processResult = ProcessDirectory(sdrSharpFolderDialog.SelectedPath);
+            // Keep showing folder dialog until a the correct folder is selected or 'Cancel' pressed
             while (processResult != ProcessResult.Success) {
                 dialogResult = sdrSharpFolderDialog.ShowDialog();
                 if (dialogResult == DialogResult.Cancel) {
@@ -59,12 +65,14 @@ namespace SDRSharpPluginManager {
             }
         }
 
-        private ProcessResult ProcessDirectory(String path) {
+        // Setting the CWD to the folder of SDR# and parse its config file
+        private ProcessResult ProcessDirectory(string path) {
             Directory.SetCurrentDirectory(path);
 
-            foreach (String requiredFileName in RequiredFiles) {
+            // Check existance of some required files in folder
+            foreach (string requiredFileName in RequiredFiles) {
                 if (!File.Exists(requiredFileName)) {
-                    String msg = String.Format("File '{0}' was not found in '{1}'", requiredFileName, path);
+                    string msg = string.Format("File '{0}' was not found in '{1}'", requiredFileName, path);
 
                     MessageBox.Show(msg, "Missing file", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
                     return ProcessResult.Missing;
@@ -77,52 +85,50 @@ namespace SDRSharpPluginManager {
         }
 
         private void LoadConfig() {
-            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
-            configMap.ExeConfigFilename = SDRSharpExe;
-            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+            // Switching the config file of 'SDRSharpPluginManager' to SDR#'s
+            AppDomain.CurrentDomain.SetData("APP_CONFIG_FILE", Path.Combine(Directory.GetCurrentDirectory(), ConfigFileName));
 
-            // Sorry, I've had no better idea
-            String sharpPluginsString = config.GetSection("sharpPlugins").SectionInformation.GetRawXml();
-            XmlDocument sharpPluginsXML = new XmlDocument();
-            sharpPluginsXML.LoadXml(sharpPluginsString);
+            // Fill listPlugins
+            NameValueCollection pluginsInConfig = (NameValueCollection)ConfigurationManager.GetSection("sharpPlugins");
+            foreach (string displayName in pluginsInConfig.Keys) {
+                string[] descriptors = pluginsInConfig[displayName].Split(',');
+                string typeName = descriptors[0];
+                string assemblyName = descriptors[1];
 
-            foreach (XmlNode node in sharpPluginsXML.GetElementsByTagName("add")) {
-                ListViewItem pluginItem = new ListViewItem(node.Attributes["key"].Value);
+                ListViewItem pluginItem = new ListViewItem(displayName);
                 listPlugins.Items.Add(pluginItem);
-
-                String[] descriptors = node.Attributes["value"].Value.Split(',');
-                String typeName = descriptors[0];
-                String assemblyName = descriptors[1];
 
                 pluginItem.SubItems.Add(typeName);
                 pluginItem.SubItems.Add(assemblyName);
             }
-
             FitColumns();
         }
         #endregion
 
-        #region Parsing plugin dll
+        #region Parsing plugin DLL
         private void btnAdd_Click(object sender, EventArgs e) {
             pluginFileDialog = new OpenFileDialog();
             pluginFileDialog.Filter = PluginFileFilter;
 
             if (pluginFileDialog.ShowDialog() == DialogResult.OK) {
-                String pluginFilePath = pluginFileDialog.InitialDirectory + pluginFileDialog.FileName;
-                LoadDll(pluginFilePath);
+                string pluginFilePath = pluginFileDialog.InitialDirectory + pluginFileDialog.FileName;
+                LoadDLL(pluginFilePath);
             }
         }
 
-        private void LoadDll(String path) {
+        private void LoadDLL(string path) {
             try {
                 Assembly pluginAssembly = Assembly.LoadFrom(path);
 
+                // Search for the first class that implements 'ISharpPlugin' interface
                 Type pluginEntryType = (from type in pluginAssembly.GetTypes()
                                         where type.GetInterface(PluginInterfaceName) != null
                                         select type).First();
-                String name = (String)pluginEntryType.GetField(DisplayNameFieldName, BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
-                String assemblyName = pluginAssembly.GetName().Name;
-                String typeName = String.Format("{0}.{1}", assemblyName, pluginEntryType.Name);
+                string name = (string)pluginEntryType.GetField(DisplayNameFieldName, BindingFlags.NonPublic | BindingFlags.Static).GetValue(null);
+                string assemblyName = pluginAssembly.GetName().Name;
+
+                // I hope generating 'typeName' is this simple
+                string typeName = string.Format("{0}.{1}", assemblyName, pluginEntryType.Name);
 
                 ListViewItem pluginItem = new ListViewItem(name);
                 listPlugins.Items.Add(pluginItem);
@@ -145,10 +151,12 @@ namespace SDRSharpPluginManager {
             System.Diagnostics.Process.Start(lnkProjectHome.Text);
         }
 
+        #region UI related methods
         private void FitColumns() {
             listPlugins.AutoResizeColumn(0, ColumnHeaderAutoResizeStyle.ColumnContent);
             listPlugins.AutoResizeColumn(1, ColumnHeaderAutoResizeStyle.ColumnContent);
             listPlugins.AutoResizeColumn(2, ColumnHeaderAutoResizeStyle.ColumnContent);
         }
+        #endregion
     }
 }
